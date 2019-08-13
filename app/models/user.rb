72278e36897c2bad1057afc0f4786116c372d6bd
TesT-1234 <your.email@example.com>
2019-08-13 16:03:08 +0900

    Make a basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase } # email属性を小文字に変換してメールアドレスの一意性を保証する
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
